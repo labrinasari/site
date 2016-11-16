fceaf3180e7e4ad6e3a0829de62d584970b3726e
andre_teles <andre_teles@SNRSL1ELBIET059>
2016-11-16 09:47:54 -0200

    opa,tudo bem

--- index.php
+++ index.php
@@ -2,32 +2,55 @@
 <html>
 <head>
 <style>
-body {margin:0;}
-
 ul {
     list-style-type: none;
     margin: 0;
     padding: 0;
     overflow: hidden;
-    background-color: white;
-    position: fixed;
-    top: 0;
-    width: 100%;
-    color: black;
+    background-color: #333;
 }
 
 li {
     float: left;
 }
 
-li a {
-    display: block;
+li a, .dropbtn {
+    display: inline-block;
+    color: white;
     text-align: center;
-        color: black;
     padding: 14px 16px;
     text-decoration: none;
 }
 
+li a:hover, .dropdown:hover .dropbtn {
+    background-color: black;
+}
+
+li.dropdown {
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #333;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+
+.dropdown-content a {
+    color: white;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #black}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
 li a:hover:not(.active) {
     background-color: #111;
 }
@@ -40,39 +63,28 @@ li a:hover:not(.active) {
 <body>
 
 <ul>
-<li><a class="active" href="#index">Home</a></li>
-  <li><a href="#prod">Produtos</a></li>
-  <li><a href="#serv">Serviços</a></li>
-  <li><a href="#cont">Contato</a></li>
-  <li><a href="#sobre">Sobre Nós</a></li>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#sobre">Sobre</a></li>
+  <li class="dropdown">
+    <a href="#" class="dropbtn">Produtos</a>
+    <div class="dropdown-content">
+      <a href="#">Notebook</a>
+      <a href="#">Teclado</a>
+      <a href="#">Mouse</a>
+      <a href="#">Smartphone</a>
+      <a href="#">TV/Tela</a>
+      <a href="#">Console</a>
+    </div>
+  </li>
+    <li><a href="#servicos">Serviços</a></li>
+  <li><a href="#faq">FAQ</a></li>
+  <li><a href="#contato">Contato</a></li>
+  <li style="float:right"><a href="#conta">Conta</a></li>
+  <li style="float:right"><a href="#carrinho">Carrinho</a></li>
 </ul>
 
-<div style="padding:20px;background-color:#898989; margin-top:30px;height:1500px;">
-<h1>Fixed Top Navigation Bar</h1>
-<h2>Scroll this page to see the effect</h2>
-<h2>The navigation bar will stay at the top of the page while scrolling</h2>
+<h3>COISAS</h3>
 
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-<p>Some text some text some text some text..</p>
-</div>
 
 </body>
 </html>
