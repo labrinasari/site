97e5175791c0d6e9d6604ef43a2c115c8a708923
sabrina_larissa <sabrina_larissa@SNRSL1ELBIET060>
2016-11-17 09:59:25 -0200

    primeiro envio contendo index

---
+++ sobre.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a, .dropbtn {
+    display: inline-block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+    background-color: black;
+}
+
+li.dropdown {
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #333;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+
+.dropdown-content a {
+    color: white;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #black}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #00d0ff;
+}
+</style>
+<img src="img/logoinfoset.png">
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#sobre">Sobre</a></li>
+  <li class="dropdown">
+    <a href="#" class="dropbtn">Produtos</a>
+    <div class="dropdown-content">
+      <a href="#">Notebook</a>
+      <a href="#">Teclado</a>
+      <a href="#">Mouse</a>
+      <a href="#">Smartphone</a>
+      <a href="#">TV/Tela</a>
+      <a href="#">Console</a>
+    </div>
+  </li>
+    <li><a href="#servicos">Serviços</a></li>
+  <li><a href="#faq">FAQ</a></li>
+  <li><a href="#contato">Contato</a></li>
+  <li style="float:right"><a href="#conta">Conta</a></li>
+  <li style="float:right"><a href="#carrinho">Carrinho</a></li>
+</ul>
+
+<p>A InfoSet foi fundada no ano de 2016 com a união de dois sócios, com o intuito de abranger</p>
+<p>o comércio eletrônico online.</p>
+<p>Além da variedade de produtos de alta qualidade, há uma ampla disponibilidade de entregas para diversos estados brasileiros, com </p>
+<p>obtenção de formas de pagamentos acessíveis aos clientes. </p>
+<p>A empresa preza por agilidade, qualidade de atendimento, velocidade de entrega e respeito ao consumidor.</p>
+
+</body>
+</html>
+
