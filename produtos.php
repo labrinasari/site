ae103c8a90e5bbc666964e739900b369ae2a4e19
andre_teles <andre_teles@SNRSL1ELBIET059>
2016-11-18 09:19:18 -0200

    opa,tudo bem

--- produtos.php
+++ produtos.php
@@ -1,8 +1,47 @@
-<?php
+<div class="img">
+  <a target="_blank" href="img/xtrax.jpg">
+ <img src='img/xtrax.jpg' width='500' height='500' onmouseover="this.src='img/xtraxop.jpg';" onmouseout="this.src='img/xtrax.jpg';" />
+  </a>
+</div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="img">
+  <a target="_blank" href="img/zenf.jpg">
+ <img src='img/zenf.jpg' width='500' height='500' onmouseover="this.src='img/zenfop.jpg';" onmouseout="this.src='img/zenf.jpg';" />
+  </a>
+</div>
 
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
