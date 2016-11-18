2b3a0a6c463bd8b443b4ae629ca42ef3786db446
sabrina_larissa <sabrina_larissa@SNRSL1ELBIET060>
2016-11-18 10:55:22 -0200

    primeiro envio contendo index

--- produtos.php
+++ produtos.php
@@ -1,8 +1,153 @@
-<?php
+<center><h3>Nossos Produtos</h3></center>
+<div class="img">
+  <a target="_blank" href="img/xtrax.jpg">
+ <img src='img/xtrax.jpg' width='500' height='500' onmouseover="this.src='img/xtraxop.jpg';" onmouseout="this.src='img/xtrax.jpg';" />
+  </a>
+</div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="img">
+  <a target="_blank" href="img/zenf.jpg">
+ <img src='img/zenf.jpg' width='500' height='500' onmouseover="this.src='img/zenfop.jpg';" onmouseout="this.src='img/zenf.jpg';" />
+  </a>
+</div>
 
+<div class="img">
+  <a target="_blank" href="img/nob.jpg">
+ <img src='img/nob.jpg' width='500' height='500' onmouseover="this.src='img/nobop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/computador.jpg">
+ <img src='img/computador.jpg' width='500' height='500' onmouseover="this.src='img/computadorop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/g4.jpg">
+ <img src='img/g4.jpg' width='500' height='500' onmouseover="this.src='img/g4op.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/note5.jpg">
+ <img src='img/note5.jpg' width='500' height='500' onmouseover="this.src='img/note5op.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/notebook.jpg">
+ <img src='img/notebook.jpg' width='500' height='500' onmouseover="this.src='img/notebookop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/peliculanote5.jpg">
+      <img src='img/peliculanote5.jpg' width='500' height='500' onmouseover="this.src='img/peliculanote5op.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/pendrivee.jpg">
+      <img src='img/pendrivee.jpg' width='500' height='500' onmouseover="this.src='img/pendriveeop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/zenfone.jpg">
+ <img src='img/zenfone.jpg' width='500' height='500' onmouseover="this.src='img/zenfoneop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/tv.png">
+ <img src='img/tv.png' width='500' height='500' onmouseover="this.src='img/tvop.jpg';" onmouseoutob="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/6s.jpg">
+ <img src='img/6s.jpg' width='500' height='500' onmouseover="this.src='img/6sop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/cadeira.jpg">
+ <img src='img/cadeira.jpg' width='500' height='500' onmouseover="this.src='img/cadeiraop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/console.jpg">
+ <img src='img/console.jpg' width='500' height='500' onmouseover="this.src='img/consoleop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/controle.jpg">
+ <img src='img/controle.jpg' width='500' height='500' onmouseover="this.src='img/controleop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+    <a target="_blank" href="img/controlexbox.jpg">
+ <img src='img/controlexbox.jpg' width='500' height='500' onmouseover="this.src='img/controlexboxop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/cooler.png">
+ <img src='img/cooler.png' width='500' height='500' onmouseover="this.src='img/coolerop.png';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/cougar.jpg">
+ <img src='img/cougar.jpg' width='500' height='500' onmouseover="this.src='img/cougarop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/ghost.png">
+ <img src='img/ghost.png' width='500' height='500' onmouseover="this.src='img/ghostop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/gtx.png">
+ <img src='img/gtx.png' width='500' height='500' onmouseover="this.src='img/gtxop.png';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/headset.jpg">
+ <img src='img/headset.jpg' width='500' height='500' onmouseover="this.src='img/headsetop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/impressora.jpg">
+ <img src='img/tv.jpg' width='500' height='500' onmouseover="this.src='img/impressoraop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/ipad.jpg">
+ <img src='img/ipad.jpg' width='500' height='500' onmouseover="this.src='img/ipadop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/macbook.jpg">
+ <img src='img/macbook.jpg' width='500' height='500' onmouseover="this.src='img/macbookop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/mouse.jpg">
+ <img src='img/mouse.jpg' width='500' height='500' onmouseover="this.src='img/mouseop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/mousepad.jpg">
+ <img src='img/mousepad.jpg' width='500' height='500' onmouseover="this.src='img/mousepadbop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/pendrive.jpg">
+ <img src='img/pendrive.jpg' width='500' height='500' onmouseover="this.src='img/pendriveop.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/playstation4.jpg">
+ <img src='img/playstation4.jpg' width='500' height='500' onmouseover="this.src='img/playstation4op.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/razer.png">
+ <img src='img/razer.png' width='500' height='500' onmouseover="this.src='img/razerop.png';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
+<div class="img">
+  <a target="_blank" href="img/motog4.jpg">
+ <img src='img/motog4.jpg' width='500' height='500' onmouseover="this.src='img/motog4op.jpg';" onmouseout="this.src='img/nob.jpg';" />
+  </a>
+</div>
