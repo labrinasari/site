85351e40a242951a2c414f0b54cd8941dff55a5a
André Luis <andre_teles@estudante.sc.senai.br>
2016-11-30 17:02:22 -0200

    Add files via upload

--- tutorial.php
+++ tutorial.php
@@ -1,8 +1,110 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<style>
+    p {
+        font-weight: bold;
+    }
+    .art {
+        font-style: normal;
+        font-size: 20;
+    }
+</style>
+<img src="img/rtg.jpg">
+<center>
+    <div class="art">Muitas pessoas esquecem que a limpeza também é um grande fator no desempenho e saúde do seu computador.Muitas vantagens podem ser descritas pelo uso eficaz da manutenção preventiva, como reduzir os riscos de quebra, envelhecimento e degeneração dos equipamentos, programar a conservação das peças, atuar antes da manutenção corretiva, amenizar os custos de compra de novos itens, entre outros. O monitoramento mensal de produtos industriais e automotivos é analisado por um pessoal qualificado e apto para resolver os possíveis problemas que aparecerão com o decorrer do tempo, uma vantagem a mais para as companhias.</div>
+    
+    <p>1° passo: Para facilitar na hora de realizar a montagem, tire uma foto da parte traseira de seu desktop;
+    <br>
+2° passo: Desconecte os cabos e limpe-os com um pano umedecido;
+<br>
+<img src="tut/">
+<br>
+3° passo: Em seguida, desparafuse a placa de proteção de seu gabinete;
+<br>
+4° passo: Assim que a parte interna esteja exposta, tire outra foto para facilitar a montagem;
+<br>
+5° passo: Desconecte o cooler; 
+<br> 
+<img src="tut/">
+<br>
+6° passo: Retire o processador;
+<br> 
+<img src="tut/">
+<br>
+7° passo: Retire a memória RAM; 
+<br> 
+<img src="tut/">
+<br>
+8° passo: Retire os cabos do HD;
+ <br>
+<img src="tut/">
+<br>
+9° passo: Retire o HD;
+<br> 
+<img src="tut/">
+<br>
+10° passo:  Retire o driver de CD;
+<br> 
+<img src="tut/">
+<br>
+11° passo: Retire os cabos da fonte;
+<br> 
+<img src="tut/">
+<br>
+12° passo: Em seguida, desparafuse a fonte e a retire;
+<br> 
+<img src="tut/">
+<br>
+13° passo: Retire os cabos do painel frontal;
+<br> 
+<img src="tut/">
+<br>
+14° passo: Retire a bateria da placa mãe;
+ <br>
+img src="tut/">
+ <br>
+15° passo: Desparafuse a placa mãe;
+ <br>
+<img src="tut/">
+<br>
+16° passo: Utilize ar comprimido para limpar a placa mãe;
+ <br>
+<img src="tut/">
+<br>
+17° passo: Utilize um pano e um pincel para limpar o gabinete e os demais componentes retirados;
+ <br>
+<img src="tut/">
+<br>
+18° passo: Agora posicione a placa mãe dentro do gabinete novamente e parafuse;  
+<br>
+<img src="tut/">
+<br>
+19° passo: Conecte os cabos do painel frontal;
+ <br>
+<img src="tut/">
+<br>
+20° passo: Reconecte a memória RAM;
+ <br>
+<img src="tut/">
+<br>
+21° passo: Posicione a fonte dentro do gabinete e parafuse;
+ <br>
+<img src="tut/">
+<br>
+22° passo: Coloque a bateria na placa mãe;
+<br>
+23° passo: Reconecte o HD;
+<br>
+24° passo: Reconecte o driver de CD;
+<br>
+25° passo: Reconecte os cabos respectivos;
+<br>
+26° passo: Encaixe o processador;
+<br>
+27° passo: Reconecte o cooler;
+<br>
+28° passo: Recoloque a placa de proteção do gabinete e parafuse;
+ <br>
+<img src="tut/">
+<br>
+29° passo: Reconecte seus periféricos.
+<br>
+<img src="tut/"></p></center>
