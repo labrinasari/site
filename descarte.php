4d5e38a0a41dfff942f4031748732855bafbd24e
andre_teles <andre_teles@SNRSL1ELBIET059>
2016-11-30 10:17:25 -0200

    pooobju

---
+++ descarte.php
@@ -0,0 +1,37 @@
+<style>
+    #img {
+        text-align: center;
+    }
+#conteudo {
+    font-size: 15px;
+}
+#titulo {
+    text-align: center;
+    font-size: 20px;
+    color: #119ced;
+    font-family:'GENISO';
+}
+</style>
+<div id="titulo">Descarte</div>
+<div id="conteudo">
+    <p>Quando novas demandas tecnológicas aparecem e sentimos necessidade de acompanhar esse avanço,
+        <br>
+    não sabemos o que fazer com aparelhos antigos já não mais desejados e impossibilitados de novos aproveitamentos. 
+   <br>
+    A InfoSet pensando não somente na responsabilidade planetária, pensa na saúde de seus clientes e amigos, 
+    <br>
+    por isso, além de ter um programa de descarte para nossa própria loja, possuímos metódos informativos,
+    <br>
+    para que nossos clientes saibam o que fazer quando se encontram nessa situação.
+    <br>
+    É necessário procurar saber sobre formas de descarte de sua cidade.
+    <br>
+    Caso não tenha um programa fixo, se informe sobre os eventos de coleta que sua cidade promove de tempos em tempos.
+    <br>
+    No caso de descate residêncial, procure uma loja fixa de informática próxima de sua casa e pergunte sobre a forma de descarte
+    <br>
+    que possuem e se podem lhe auxiliar.</p>
+</div>
+<div id="img">
+<img src="img/descarte.png"/>
+</div>
--- home.php
+++ home.php
@@ -27,3 +27,11 @@ foreach ($dadosProduto as $rowProduto) {
     <?php
 }
 ?>
+<br>
+<center><h3><a href="tutorial.php">Quer aprender à fazer uma manutenção preventiva em casa? Confira nosso artigo!</a></h3></center>
+<p>Muitas pessoas esquecem que a limpeza também é um grande fator no desempenho e saúde do seu computador.
+    <br>
+    Muitas vantagens podem ser descritas pelo uso eficaz da manutenção preventiva, como reduzir os riscos de quebra, envelhecimento e degeneração dos equipamentos, programar a conservação das peças, atuar antes da manutenção corretiva, amenizar os custos de compra de novos itens, entre outros. O monitoramento mensal de produtos industriais e automotivos é analisado por um pessoal qualificado e apto para resolver os possíveis problemas que aparecerão com o decorrer do tempo, uma vantagem a mais para as companhias.
+    <br>
+    <b><a href="tutorial.php">Leia Mais!</a></b>
+</p>
